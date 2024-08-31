cc4c8ec51c8121b4e0fc81060670409d3b7d3160
bhanudova02 <bhanudova02@gmail.com>
2024-08-31 15:30:48 +0530

    new ui changes added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,10 +11,9 @@ export default function Experience() {
                     </div>
                     <div className="lg:absolute bottom-0 px-2 lg:px-6">
                         <h6 className="font-semibold text-2xl">Learn About Us</h6>
-                        <h2 className="text-5xl font-bold mt-1">We Provide Reliable And Effective Legal Services</h2>
+                        <h2 className="text-3xl md:text-5xl font-bold mt-1">We Provide Reliable And Effective Legal Services</h2>
                         <p className="text-lg mt-4">
-
-                            With 16 years of experience, we deliver reliable legal services with precision and care. Our team offers personalized solutions and guides you through every step with integrity. Choose us and gain a trusted advocate for your cause.
+                        Our 16 years of experience provide the foundation you can trust. We specialize in delivering reliable and effective legal services, ensuring that your rights are defended with precision and care. Our team is committed to offering personalized legal solutions, guiding you through every step of the legal process with integrity and dedication. When you choose us, you're not just getting a lawyer; you're gaining a trusted advocate for your cause.
                         </p>
                     </div>
                 </div>
