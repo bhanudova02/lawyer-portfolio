b6e16d359dc61f9f3dc540240f3d7b77bf6933dc
bhanudova02 <bhanudova02@gmail.com>
2024-09-01 11:20:00 +0530

    new animations added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,9 +18,9 @@ export default function Experience() {
                         </div>
                         <div data-aos="fade-up" data-aos-delay="400" className="px-2 lg:px-6 mt-0 lg:mt-8">
                             <div>
-                                <h6 className="font-semibold text-2xl">Learn About Us</h6>
-                                <h2 className="text-3xl md:text-5xl font-bold mt-1">We Provide Reliable And Effective Legal Services</h2>
-                                <p className="text-lg mt-4">
+                                <h6 data-aos="fade-up" data-aos-delay="600" className="font-semibold text-2xl">Learn About Us</h6>
+                                <h2 data-aos="fade-up" data-aos-delay="800" className="text-3xl md:text-5xl font-bold mt-1">We Provide Reliable And Effective Legal Services</h2>
+                                <p data-aos="fade-up" data-aos-delay="1000" className="text-lg mt-4">
                                     Our 16 years of experience provide the foundation you can trust. We specialize in delivering reliable and effective legal services, ensuring that your rights are defended with precision and care. Our team is committed to offering personalized legal solutions, guiding you through every step of the legal process with integrity and dedication. When you choose us, you're not just getting a lawyer; you're gaining a trusted advocate for your cause.
                                 </p>
                             </div>
