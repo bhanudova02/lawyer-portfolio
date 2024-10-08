e932a38c814b3577b98e2d6ab7d9c8549aa52129
Mr.Bhanu <bhanudova02@gmail.com>
2024-10-08 20:59:59 +0530

    Update WhatsApp.jsx

--- src/components/WhatsApp.jsx
+++ src/components/WhatsApp.jsx
@@ -5,14 +5,11 @@ export function WhatsApp() {
     const message = 'Hello, I would like to chat with you!'; // Optional initial message
 
     return (
-        <a
-            href={`/`}
-            
-            rel="noopener noreferrer"
+        <div
             className="fixed bottom-4 right-4 bg-green-500 p-3 rounded-full shadow-lg flex items-center space-x-2 cursor-pointer z-50 hover:bg-green-600 transition-colors duration-300"
         >
             <FaWhatsapp className="text-white text-2xl" />
             <span className="text-white font-semibold text-sm sm:inline">Chat with us</span>
-        </a>
+        </div>
     );
 }
